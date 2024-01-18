a5d4771441719a2409874090c696e9d95597ac17
rielisson <118862671+rielisson@users.noreply.github.com>
2024-01-18 17:31:42 -0300

    versão 2.0 completa

--- script.js
+++ script.js
@@ -14,12 +14,18 @@ class ListaDeTarefas{
     displayItems(){
         const listElements = document.getElementById('tela');
         listElements.innerHTML = '';
-
+        
         this.list.forEach(item => {
             const listItem = document.createElement('li');
             listItem.textContent = item;
             listElements.appendChild(listItem);
         })
+    }
+    clearItems(){
+        const index = this.list.length;
+        if(index != 0){
+            this.list.splice(0,index);
+        }
     }
 }
 const userList = new ListaDeTarefas();
@@ -30,6 +36,7 @@ function addItem(){
     if(newItem.trim() !== ''){
         userList.adicionar(newItem);
         userList.displayItems();
+        document.querySelector('input').value = '';
     }
 }
 function removerItem(){
@@ -37,5 +44,10 @@ function removerItem(){
     if(dlt.trim() !== ''){
         userList.remover(dlt);
         userList.displayItems();
+        document.querySelector('input').value = '';
     }
+}
+function zeroItems(){
+    userList.clearItems();
+    userList.displayItems();
 }
